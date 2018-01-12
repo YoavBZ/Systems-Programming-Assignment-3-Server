c949b7c560d47dea6a7fdbdee6e3eb1db4ada620
Yoav <yoav@idomoo.com>
2018-01-12 17:45:11 +0200

    Added test files

---
+++ src/test/java/bgu/spl181/net/impl/ReactorServerClientBasicTest.java
@@ -0,0 +1,22 @@
+package bgu.spl181.net.impl;
+
+import org.junit.Test;
+
+public class ReactorServerClientBasicTest extends ReactorServerClientTestTemplate {
+	@Test(timeout = 50000)
+	public void ReactorServerClientBasicTest() {
+		server = A.initiateServer("DemoReactor", A.serverPort);
+
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Thread[] clients = A.initiateClients(1, A.serverIp, A.serverPort,
+				"./src/test/resources/Basic_Server_Client_Test/", null, null);
+
+		A.waitForClients(clients);
+
+	}
+}
